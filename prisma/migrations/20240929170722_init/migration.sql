1ee15bafbc746bae56b6819e87d641b11901e035
Hasan Rahman <hasanrahman33605@gmail.com>
2024-09-29 23:40:29 +0600

    setup project and create category controller

---
+++ prisma/migrations/20240929170722_init/migration.sql
@@ -0,0 +1,49 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Sale" (
+    "id" SERIAL NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "saleDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Sale_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Sale" ADD CONSTRAINT "Sale_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/controllers/categoryController.js
@@ -0,0 +1,57 @@
+const prisma = require('../models/prismaClient')
+
+
+// create category
+exports.createCategory = async(req, res) =>{
+    const { name } = req.body;
+    try {
+        const category = await prisma.category.create({
+            data: { name }
+        });
+        res.json(category)
+
+    } catch (error) {
+        res.status(400).json({ error: 'Error creating category' });
+    }
+}
+
+//find all
+exports.getCategoris =async (req, res) => {
+    const categoris = await prisma.category.findMany();
+    res.json(categoris);
+}
+
+//categoris by id
+exports.getCategoryById = async (req,res) =>{
+    const {id} = req.params;
+    const category = await prisma.category.findUnique({
+        where:{id: parseInt(id)}
+    })
+    res.json(category);
+}
+
+
+//categoris by id
+exports.updateCategory = async (req,res) =>{
+    const {id} = req.params;
+    const {name} =req.body;
+
+    const category = prisma.category.update({
+        where:{id: parseInt(id)},
+        data:{name},
+    })
+
+    res.json(category)
+}
+
+
+//delete
+
+exports.deleteCategory = async (req,res) =>{
+    const {id} = req.params;
+   await prisma.category.delete({
+        where:{id: parseInt(id)}
+    })
+
+    res.json({message:'Category deleted' })
+}
